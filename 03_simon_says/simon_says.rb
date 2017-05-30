b8e4f960df84f412936227746d35f0c01c4ed6db
Qin Chen <hyathynth@gmail.com>
2017-05-30 14:48:19 -0500

    complete exercise 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+def echo string
+	string
+end
+
+def shout string
+	string.upcase
+end
+
+def repeat string, i = 2
+	([string] * i).join(' ')
+end
+
+def start_of_word word, num
+	word[0,num]
+end
+
+def first_word word
+	word.split.first
+end
+
+def titleize word
+	temp_word = word.split(' ')
+	temp_word.each do |part|
+		part.capitalize!
+		if part == "And" or part == "The" or part == "Over"
+			part.downcase!
+		end
+	end
+	if temp_word.first == "the" or temp_word.first == "and" or temp_word.first == "over"
+		temp_word.first.capitalize!
+	end
+	word = temp_word.join(' ')
+end
