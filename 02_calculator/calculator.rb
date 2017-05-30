e63aecedaac025c9e4895a6c004b97efb724736c
Qin Chen <hyathynth@gmail.com>
2017-05-30 13:30:44 -0500

    finish exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+def add x, y
+	x + y
+end
+
+
+def subtract x, y
+	x - y
+end
+
+
+def sum array
+	total = 0
+	array.each do |ele|
+		total += ele
+	end
+	total
+end
+
+
+def multiply x, y
+	x * y
+end
+
+
+def power x, y
+	x ** y
+end
+
+def factorial num
+	sum = 1
+	while (num > 1)
+		sum *= num
+		num -=1
+	end
+	sum
+end
