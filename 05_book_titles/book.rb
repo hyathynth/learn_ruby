99b8e471fe8d96ee4d40a9b9c3693c0f8c839036
Qin Chen <hyathynth@gmail.com>
2017-06-05 19:29:25 -0400

    complete exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+	attr_reader :title
+
+	def title= (name)
+		new_name = name.split(" ")
+		exception = ["and", "do", "the", "a", "an", "in", "of"]
+
+		new_name.each do |word|
+			if !exception.include? word
+				word.capitalize!
+			end
+		end
+		if new_name.first == "the"
+			new_name.first.capitalize!
+		end
+		@title = new_name.join(" ")
+		
+	end
 end
+
